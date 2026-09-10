079985360ac5be07926578b7e0bc5576f33a102c
Akbar <akbar.code20@gmail.com>
2026-09-10 21:55:27 +0530

    first commit

---
+++ server/src/server.js
@@ -0,0 +1,19 @@
+
+require("dns").setServers(["8.8.8.8", "1.1.1.1"]);
+require("dotenv").config();
+
+const app = require("./app");
+const connectDB = require("./config/db");
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => { 
+    await connectDB();
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+};
+
+
+startServer();
